24f6e455070b5e02fc74018b5e64da2ca4aa6201
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2024-07-27 05:42:13 +0700

    update

--- sample-microservice-hello/src/main/java/lab/microservice/hello/SpringGreetApplication.java
+++ sample-microservice-hello/src/main/java/lab/microservice/hello/SpringGreetApplication.java
@@ -1,11 +1,11 @@
-package lab.microservice.currencyconversion;
+package lab.microservice.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableEurekaClient
 public class SpringGreetApplication {
 
 	public static void main(String[] args) {
