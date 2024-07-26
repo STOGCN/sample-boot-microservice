6df258917a8dbf01ac313c2b6bc5c035dafc9536
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2024-07-27 05:47:11 +0700

    update

--- sample-microservice-hello/src/main/java/lab/microservice/hello/SpringGreetApplication.java
+++ sample-microservice-hello/src/main/java/lab/microservice/hello/SpringGreetApplication.java
@@ -2,10 +2,8 @@ package lab.microservice.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class SpringGreetApplication {
 
 	public static void main(String[] args) {
